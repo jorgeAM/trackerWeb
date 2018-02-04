ad3449aa4e499c9b07b1b73d7b368bce91d7a01e
Jorge L <jorge.alfmur@gmail.com>
2018-02-04 13:59:39 -0500

    conexion con firebase y dibujar mapa con coordenadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+  usuarios: Observable<any[]>;
+  nombre: string;
+  siguiendo: boolean = false;
+
+  constructor(private db: AngularFireDatabase){
+    this.usuarios = db.list('usuarios').valueChanges();
+  }
+
+  elegir(usuario:any){
+    this.nombre = usuario.nombre;
+    this.lat = usuario.latitud;
+    this.lng = usuario.longitud;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { AgmCoreModule } from '@agm/core';
+import { FirebaseConfig } from "../credentials/firebase";
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(FirebaseConfig.firebase),
+    AngularFireDatabaseModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCSz59nTIHYr0OCvfgk1Z0H6SLE6RijWzk'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
